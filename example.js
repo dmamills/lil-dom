89f630ae8a913344107d5ee3bbca7ead8ec78e76
Daniel Mills <mills.dma@gmail.com>
2017-06-12 19:04:23 -0400

    bit of example page cleanup

--- example.js
+++ example.js
@@ -11,11 +11,14 @@ var styles = ld('style', `
  }
 
  pre {
+  border-radius: 5px;
   padding: 0.8rem;
   background: lightgrey;
  }
 
  header {
+   background-color: #30373b;
+   color: #fff;
    padding: 1.5rem;
  }
 
@@ -40,13 +43,107 @@ a.nav-item {
   text-transform: uppercase;
 }
 
+footer {
+  color: #fff;
+  background-color: #30373b;
+  border-top: 1px solid black;
+  padding: 1rem;
+}
+
+a, a:visited {
+  font-weight: 400;
+  color: #30373b;
+}
+
+footer a, footer a:visited {
+  color: #fff;
+}
+
+footer p {
+  margin: 0;
+}
+
 `);
+
+
+
+//handy for quick linkin'
+function anchor(href, text) {
+  return ld('a', { href }, text);
+}
+
+/**
+ * Basic Section
+ */
+var basic = { id: 'basic' };
+basic.content =  ld('p', 'lil-dom is a just another h clone. inspired by ',
+   anchor('https://github.com/hyperhype/hyperscript', 'hyperscript'),
+  ' and the ',
+   anchor('https://github.com/hyper2/h2spec', 'h2 spec.'),
+  ' It\'s a simple way to build out dom nodes, this entire example page is built with it!'
+)
+
+basic.examples = `// h(<tagname>, ...(<string> | <options> | <Array>)
+ld('h1', 'Some header')
+ld('h1.primary', 'header with class')
+ld('h1#primary', 'header with id')
+ld('.primary', 'defaults to div')
+ld('.primary.secondary', 'wow two classes!')
+`;
+
+basic.results = `<h1>Some Header</h1>
+<h1 class="primary">header with class</h1>
+<h1 id="primary">header with id</h1>
+<div class="primary">defaults to div</div>
+<div class="primary secondary">wow two classes!</div>
+`;
+
+/**
+ * Options Section
+ */
+var options = {id: 'options' };
+options.content = ld('p', `The second parameter to the function allows you to pass options to your dom node. Style can be provided as an object and will be inlined.`);
+options.examples =`ld('a', { href: 'http://example.com/' }, 'a link.')
+ld('span', { style: { 'font-weight': '600' } }, 'heavy')
+ld('.cool', { 'data-name': 'fun!' } })
+`;
+
+options.results = `<a href="http://example.com/">a link</a>
+<span style="font-weight: 600;">heavy</span>
+<div class="cool" data-name="fun!"></div>
+`
+
+/**
+ * Events section
+ */
+var events = {id: 'events' };
+events.content = ld('p', 'Events handlers can be bound by passing in the attributes object. Note: handler functions are not inlined, but attached through addEventListener.');
+events.examples = `ld('button', { 'onclick': (e) => { console.log('clicked me!'); }, 'click me')`
+events.results = `<button>click me</button>`;
+
+/**
+ * Events section
+ */
+var nesting = { id: 'nesting' };
+nesting.content = ld('p', 'Any element passed as an argument will be appended as a child node. If any array is found it will be iterated over and append any nodes found there.');
+nesting.examples = `ld('p', 'My paragraph has a ', ld('a', { href: 'http://example.com/'}, 'link'), 'in the middle!.')
+ld('.parent', [
+  [1,2,3,4,5].map(v => ld('.kid', v.toString)
+])`
+nesting.results = `<p>My paragraph has a <a href="http://example.com/">link</a> in the middle!</p>
+<div class="parent">
+  <div class="kid">1</div>
+  <div class="kid">2</div>
+  <div class="kid">3</div>
+  <div class="kid">4</div>
+  <div class="kid">5</div>
+</div>`
 
 
 /**
  * Build out an example section, content should be a node
  */
-function buildSection(id, title, content, src, result) {
+function renderSection(id, title, content, src, result) {
   return ld(`section#${id}`, [
     ld('h2', `${title}`),
     content,
@@ -57,84 +154,28 @@ function buildSection(id, title, content, src, result) {
   ]);
 }
 
-/**
- * Basic Section
- */
-var basicContent =  ld('p', ld('p', 
-   `lil-dom is a just another h clone. inspired by `,
-   ld('a', {href: 'https://github.com/hyperhype/hyperscript'}, 'hyperscript'),
-  ' and the ',
-   ld('a', { href:'https://github.com/hyper2/h2spec'}, 'h2 spec.'),
-  ' It\'s a simple way to build out dom nodes, this entire example page is built with it.'
-))
-
-var basicSection = buildSection('basic', 'Basic', basicContent, `// h(<tagname>, ...(<string> | <options> | <Array>)
-ld('h1', 'Some header')
-ld('h1.primary', 'header with class')
-ld('h1#primary', 'header with id')
-ld('.primary', 'defaults to div')
-ld('.primary.secondary', 'wow two classes!')
-`, `<h1>Some Header</h1>
-<h1 class="primary">header with class</h1>
-<h1 id="primary">header with id</h1>
-<div class="primary">defaults to div</div>
-<div class="primary secondary">wow two classes!</div>
-`);
-
-
-/**
- * Options Section
- */
-var optionsContent = ld('p', `The second parameter to the function allows you to pass options to your dom node. Style can be provided as an object and will be inlined.`);
-var optionsExamples =`ld('a', { href: 'http://example.com/' }, 'a link.')
-ld('span', { style: { 'font-weight': '600' } }, 'heavy')
-ld('.cool', { 'data-name': 'fun!' } })
-`;
-
-var optionsResults = `<a href="http://example.com/">a link</a>
-<span style="font-weight: 600;">heavy</span>
-<div class="cool" data-name="fun!"></div>
-`
-
-var optionsSection = buildSection('options', 'Options', optionsContent, optionsExamples, optionsResults);
-
-/**
- * Events section
- */
-var eventsContent = ld('p', 'Events handlers can be bound by passing in the attributes object. Note: handler functions are not inlined, but attached through addEventListener.');
-
-var eventsExamples = `ld('button', { 'onclick': (e) => { console.log('clicked me!'); }, 'click me')`
-var eventsResults = `<button>click me</button>`;
-var eventsSection = buildSection('events', 'Events', eventsContent, eventsExamples, eventsResults);
-
-var nestingContent = ld('p', 'Any element passed as an argument will be appended as a child node. If any array is found it will be iterated over and append any nodes found there.');
-var nestingExamples = `ld('p', 'My paragraph has a ', ld('a', { href: 'http://example.com/'}, 'link'), 'in the middle!.')
-ld('.parent', [
-  [1,2,3,4,5].map(v => ld('.kid', v.toString)
-])`
-var nestingResults = `<p>My paragraph has a <a href="http://example.com/">link</a> in the middle!</p>
-<div class="parent">
-  <div class="kid">1</div>
-  <div class="kid">2</div>
-  <div class="kid">3</div>
-  <div class="kid">4</div>
-  <div class="kid">5</div>
-</div>`
-var nestingSection = buildSection('nesting', 'Nesting', nestingContent, nestingExamples, nestingResults);
-
+//Render each section
+var sections = [basic, options, events, nesting].map(s => {
+  var title = s.id[0].toUpperCase() + s.id.substr(1)
+  s.html = renderSection(s.id, title, s.content, s.examples, s.results);
+  return s;
+});
 
 //"Nav bar"
 var nav = ld('nav', [
-  ['basic','options','events','nesting'].map(n => ld('a.nav-item', { href: `#${n}` }, n))
+  sections.map(s => s.id).map(n => ld('a.nav-item', { href: `#${n}` }, n))
+]);
+
+var footer = ld('footer', [
+  ld('p', 'Made by ', anchor('http://yomills.com', 'daniel mills'), ' check it out on ', anchor('https://github.com/dmamills/lil-dom', 'github')) 
 ]);
 
 // The webpage constructed
 var examplePage = ld('.example-page', [
   ld('header', [ ld('h1', 'lil-dom') ]),
   nav,
-  ld('.sections', [ basicSection, optionsSection, eventsSection, nestingSection ])
-]);
-
+  ld('.sections', sections.map(s => s.html)),
+], footer);
 
 //mount it up
 document.head.appendChild(styles);
